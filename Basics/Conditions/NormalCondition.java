98d932bbdea4ba1a98ae9a0c89ddaf6b45f9929b
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-01-28 14:59:53 +0530

    just very basics logics

---
+++ Basics/Conditions/NormalCondition.java
@@ -0,0 +1,23 @@
+package Basics.Conditions;
+
+public class NormalCondition {
+
+    public static void main(String[] args) {
+        // int num = 10 ;
+        // if(num<10 || num>10){
+        //     System.out.println(true);
+        // }
+        // else{
+        //     System.out.println(false);
+        // }
+
+        int x= 6;
+        int y =10;
+        if(x>y){
+            System.out.println(x);
+        }
+        else{
+            System.out.println(y);
+        }
+    }
+}
