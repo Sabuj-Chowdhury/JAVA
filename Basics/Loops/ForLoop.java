a23be7e3d8f59212f1d427b66a4c8fe2ea810ec8
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-01-28 18:21:01 +0530

    variors Loops

---
+++ Basics/Loops/ForLoop.java
@@ -0,0 +1,46 @@
+ package Basics.Loops;
+
+class ForLoop {
+
+  public static void main(String[] args) {
+   
+    // while(true){
+    //     System.out.println("hello");
+    // }
+    // // for(int i= 0;i<10;i++){
+    // //     System.out.println(i);
+    // // }
+    // // int i = 1;
+    // // while (i<10) {
+    // //     System.out.println(i);
+    // //     i++;
+        
+    // // }
+    // // System.out.println(i);
+    // int i = 1;
+    // while (i<10) {
+
+    //     System.out.println(i);
+    //     int j= 1;
+    //     while (j<=3) {
+    //         System.out.println("hi");
+    //         j++;
+    //     }
+    //     i++;
+        
+    // }
+    // System.out.println(i);
+
+    // int i = 5;
+    // do{
+    //     System.out.println("hi");
+    //     i++;
+    // }while(i<4);
+    // System.out.println(i);
+
+    // for (int i= 7 ;i>=0;i-- )
+    // {
+    //     System.out.println("Day"+i);
+    // }
+  }
+}
